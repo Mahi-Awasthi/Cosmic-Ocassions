87e0bc0a83242c8b3a025961117c7916dc00e032
MahiAwasthi <mahi2057.be23@chitkara.edu.in>
2024-10-24 11:06:40 +0530

    .

--- React/Cosmic-App/src/App.jsx
+++ React/Cosmic-App/src/App.jsx
@@ -5,10 +5,13 @@ import './Home.css';
 import './About.css'; 
 import './Services.css'; 
 import './Contact.css'; 
+import './ExpenseTracker.css'; 
 import Home from './assets/Home';
 import About from './assets/About';
 import Services from './assets/Services';
 import Contact from './assets/Contact';
+import ExpenseTracker from './assets/ExpenseTracker';
+
 
 const App =() => {
   return (
@@ -18,6 +21,7 @@ const App =() => {
       <Route path="/about" element={<About />} />
       <Route path="/Services" element={<Services />} />
       <Route path="/Contact" element={<Contact />} />
+      <Route path="/ExpenseTracker" element={<ExpenseTracker />} />
        
     </Routes>
   </Router>
